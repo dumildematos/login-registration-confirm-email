32aa834841a56d115212ebefcf055b46044e0e87
Dumilde Matos <dumildejosematos@gmail.com>
2022-02-04 13:47:25 +0100

    first commit

---
+++ src/main/java/com/example/loginregistrationconfirmemail/LoginRegistrationConfirmEmailApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginregistrationconfirmemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegistrationConfirmEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRegistrationConfirmEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/loginregistrationconfirmemail/LoginRegistrationConfirmEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginregistrationconfirmemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegistrationConfirmEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
