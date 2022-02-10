7c71ae9d61a06ae6a78b70a955b3180588dc492e
Dumilde Matos <dumildejosematos@gmail.com>
2022-02-10 21:20:40 +0100

    Add user app service structure

---
+++ src/main/java/com/example/loginregistrationconfirmemail/appuser/AppUserRepository.java
@@ -0,0 +1,14 @@
+package com.example.loginregistrationconfirmemail.appuser;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface AppUserRepository {
+
+    Optional<AppUser> findByEmail(String email);
+
+}
---
+++ src/main/java/com/example/loginregistrationconfirmemail/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.example.loginregistrationconfirmemail.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
