5809078f6db66f72bdc213f8b0077aedb95a6522
Dumilde Matos <dumildejosematos@gmail.com>
2022-02-11 21:11:17 +0100

    Fix permited url for registration

--- src/main/java/com/example/loginregistrationconfirmemail/registration/RegistrationController.java
+++ src/main/java/com/example/loginregistrationconfirmemail/registration/RegistrationController.java
@@ -2,6 +2,7 @@ package com.example.loginregistrationconfirmemail.registration;
 
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ public class RegistrationController {
 
     private RegistrationService registrationService;
 
+    @PostMapping
     public String register(@RequestBody RegistrationRequest request){
         return registrationService.register(request);
     }
--- src/main/java/com/example/loginregistrationconfirmemail/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/loginregistrationconfirmemail/security/config/WebSecurityConfig.java
@@ -24,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                    .antMatchers("/api/v*/registration/*")
+                    .antMatchers("/api/v*/registration/**")
                     .permitAll()
                 .anyRequest()
                 .authenticated().and()
